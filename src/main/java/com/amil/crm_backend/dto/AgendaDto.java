24a30c547a999e05efa583a293f7c34371920141
kevengarcyya <kevengarcyya@gmail.com>
2026-03-26 21:40:19 -0300

    feat: AgendaEntity e AgendaDto

--- src/main/java/com/amil/crm_backend/dto/AgendaDto.java
+++ src/main/java/com/amil/crm_backend/dto/AgendaDto.java
@@ -1,4 +1,23 @@
 package com.amil.crm_backend.dto;
 
+import com.amil.crm_backend.entity.AgendaEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
+@NoArgsConstructor
+@Setter
+@Getter
 public class AgendaDto {
+
+    private Long id;
+    private String nome;
+    private float valor;
+    private String descricao;
+    private String status;
+
+    public AgendaDto(AgendaEntity agendaEntity){
+        BeanUtils.copyProperties(agendaEntity, this);
+    }
 }
--- src/main/java/com/amil/crm_backend/entity/AgendaEntity.java
+++ src/main/java/com/amil/crm_backend/entity/AgendaEntity.java
@@ -1,6 +1,33 @@
 //HORARIOS
 package com.amil.crm_backend.entity;
 
+import com.amil.crm_backend.dto.AgendaDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
+@Table(name = "agenda")
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
 public class AgendaEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private float valor;
+    @Column(nullable = false)
+    private String descricao;
+    @Column(nullable = false)
+    private String status;
+
+    public AgendaEntity(AgendaDto agendaDto){
+        BeanUtils.copyProperties(agendaDto, this);
+    }
 }
