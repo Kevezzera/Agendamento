eb70a19f75b395835144962277eab78472c0b170
kevengarcyya <kevengarcyya@gmail.com>
2026-03-28 23:43:25 -0300

    feat: implement AgendaController.

--- src/main/java/com/amil/crm_backend/controller/AgendaController.java
+++ src/main/java/com/amil/crm_backend/controller/AgendaController.java
@@ -1,4 +1,22 @@
 package com.amil.crm_backend.controller;
 
+import com.amil.crm_backend.dto.AgendaDto;
+import com.amil.crm_backend.service.AgendaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/agenda")
+@RestController
 public class AgendaController {
+
+    @Autowired
+    private AgendaService agendaService;
+
+    @PostMapping
+    public void save(@RequestBody AgendaDto agendaDto){
+        agendaService.save(agendaDto);
+    }
 }
